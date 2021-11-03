8dac6a71ef9b995bfbf5e318a70f5b5c5639bf0d
unknown <saounmustafa@gmail.com>
2021-11-03 21:12:39 +0500

    3rd-commit

--- App.js
+++ App.js
@@ -8,25 +8,44 @@ export default function App() {
   const [fP, setfP] = React.useState(0);
 
   React.useEffect(() => {
-    let discounted = (op * discount) / 100;
-    let newPrice = op - discounted;
+    let discounted = ((op * discount) / 100).toFixed(2);
+    let newPrice = (op - discounted).toFixed(2);
+
     setfP(newPrice);
     setSave(discounted);
   });
 
+  const checkDiscountVal = (n) => {
+    if (n >= 0 && n <= 100) {
+      setDiscount(n);
+    } else {
+      alert("Discount cannot be greater than 100!");
+    }
+  };
+  const checkOpVal = (a) => {
+    if (a > 0) {
+      setOp(a);
+    } else {
+      alert("Enter a postive Value");
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Discount Calculator</Text>
+      <Text style={{ fontSize: 30 }}>Discount Calculator</Text>
       <TextInput
+        style={styles.fields}
         placeholder="Enter Original Price"
-        onChangeText={(val) => setOp(val)}
+        keyboardType="number-pad"
+        onChangeText={(val) => checkOpVal(val)}
       />
       <TextInput
+        style={styles.fields}
         placeholder="Enter Discount %"
-        onChangeText={(val) => setDiscount(val)}
+        keyboardType="number-pad"
+        onChangeText={(val) => checkDiscountVal(val)}
       />
-      <Text>You saved: {save}</Text>
-      <Text>Final price: {fP}</Text>
+      <Text style={styles.label}>You saved: {save}</Text>
+      <Text style={styles.label}>Final price: {fP}</Text>
     </View>
   );
 }
@@ -37,5 +56,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  fields: {
+    height: "8%",
+    width: "60%",
+    margin: 5,
+    paddingLeft: 10,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    borderColor: "#023d3c",
+    backgroundColor: "#d9fcfc",
+  },
+  label: {
+    marginTop: 10,
+    fontSize: 25,
   },
 });
