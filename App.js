c1e110a00d805dd802349ef229c9ad73de82db52
unknown <saounmustafa@gmail.com>
2021-11-03 20:34:37 +0500

    2nd-commit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [op, setOp] = React.useState(0);
+  const [discount, setDiscount] = React.useState(0);
+  const [save, setSave] = React.useState(0);
+  const [fP, setfP] = React.useState(0);
+
+  React.useEffect(() => {
+    let discounted = (op * discount) / 100;
+    let newPrice = op - discounted;
+    setfP(newPrice);
+    setSave(discounted);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Discount Calculator</Text>
+      <TextInput
+        placeholder="Enter Original Price"
+        onChangeText={(val) => setOp(val)}
+      />
+      <TextInput
+        placeholder="Enter Discount %"
+        onChangeText={(val) => setDiscount(val)}
+      />
+      <Text>You saved: {save}</Text>
+      <Text>Final price: {fP}</Text>
     </View>
   );
 }
@@ -14,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
